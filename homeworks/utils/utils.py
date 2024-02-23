ef9cbfa3d83b91fe68a06804e18f2b752d2fc848
jejuHyeop <sh.han@yoil.co.kr>
2024-02-23 10:11:56 +0900

    sh11

--- homeworks/utils/utils.py
+++ homeworks/utils/utils.py
@@ -22,6 +22,7 @@ def intro(st, time, line=[]):
 
 
 def valid_input(st, boundary, return_type, time=None):
+
     while True:
         if time == None:
             user_input = input(st).strip()
@@ -30,10 +31,12 @@ def valid_input(st, boundary, return_type, time=None):
                 user_input = inputimeout(prompt=st, timeout=time).strip()
             except TimeoutOccurred:
                 print("입력 시간 초과!!")
-                return None   
+                return None
+        
         if user_input == 'q':
             print("프로그램 강제 종료 !!")
-            exit(1) 
+            exit(1)
+            
         if return_type == int:
             try:
                 if "." in user_input:
@@ -42,8 +45,10 @@ def valid_input(st, boundary, return_type, time=None):
                     user_input = int(user_input)
             except:
                 print("숫자만 입력해주세요!!")
+
         if user_input in boundary:
             return user_input
+
         print("입력값이 유효하지 않습니다!!")
         input("\n\nRETRY [ENTER]")
         system("clear")
